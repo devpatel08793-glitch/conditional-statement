670a2732f5ed6873146669024f2cfa8ed19dac1d
Dev Savsani <devpatel08793@gmail.com>
2026-01-09 11:04:35 +0530

    name presence check using list or if , else statement

---
+++ use list and conditional statement.py
@@ -0,0 +1,7 @@
+l = ["dev", "harsh", "meet", "bhumit", "deep"]
+name = input("Enter your name :")
+if name in l:
+    print("your name is present in the list")
+else:
+    print("your name is not present in the list")
+        
